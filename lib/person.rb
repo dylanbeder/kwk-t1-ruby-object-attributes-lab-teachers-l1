f8286466f8a6d481210ce97dc00cd3d498d288bc
dylanbeder <dylan.beder@duke.edu>
2018-06-16 21:16:25 +0000

    automatically backed up by learn

---
+++ lib/person.rb
@@ -0,0 +1,21 @@
+class Dog
+  def name=(persons_name)
+    @name=persons_name
+  end
+  
+  def name
+    @name
+  end
+  
+  def breed=(persons_job)
+    @job=persons_job
+  end
+  
+  def breed
+    @job
+  end
+end
+
+lassie=Person.new
+lassie.name = "Lassie"
+puts lassie.name
